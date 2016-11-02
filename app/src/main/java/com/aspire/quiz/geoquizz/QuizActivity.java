c725b890cc124030ffa1d9e652436d512e0264eb
Thomas Phillips <tpphilli@amazon.com>
2016-11-02 14:22:10 -0700

    Basic interface with no functionality

--- app/src/main/java/com/aspire/quiz/geoquizz/QuizActivity.java
+++ app/src/main/java/com/aspire/quiz/geoquizz/QuizActivity.java
@@ -1,13 +1,35 @@
 package com.aspire.quiz.geoquizz;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class QuizActivity extends AppCompatActivity {
+
+    private Button mTrueButton;
+    private Button mFalseButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz);
+
+        mTrueButton = (Button) findViewById(R.id.trueButton);
+        mFalseButton = (Button) findViewById(R.id.falseButton);
+
+        mTrueButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        mFalseButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
